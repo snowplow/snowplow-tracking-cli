40e69b3edb8b3f1e2d376980bc6fcb09a59e2d9b
jbeemster <joshua.a.beemster@gmail.com>
2018-08-17 18:22:46 +0200

    Format and lint project (closes #12)

--- snowplowtrk_test.go
+++ snowplowtrk_test.go
@@ -14,158 +14,158 @@
 package main
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
-  "github.com/jarcoal/httpmock"
-  "net/http"
-  gt "gopkg.in/snowplow/snowplow-golang-tracker.v1/tracker"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	gt "gopkg.in/snowplow/snowplow-golang-tracker.v1/tracker"
+	"net/http"
+	"testing"
 )
 
 // --- CLI
 
-func TestGetSdJson(t *testing.T) {
-  assert := assert.New(t)
+func TestGetSdJSON(t *testing.T) {
+	assert := assert.New(t)
 
-  sdj, err := getSdJson("", "", "")
-  assert.Nil(sdj)
-  assert.NotNil(err)
-  assert.Equal("FATAL: A --sdjson or a --schema URI plus a --json needs to be specified.", err.Error())
+	sdj, err := getSdJSON("", "", "")
+	assert.Nil(sdj)
+	assert.NotNil(err)
+	assert.Equal("FATAL: A --sdjson or a --schema URI plus a --json needs to be specified.", err.Error())
 
-  sdj, err = getSdJson("", "iglu:com.acme/event/jsonschema/1-0-0", "")
-  assert.Nil(sdj)
-  assert.NotNil(err)
-  assert.Equal("FATAL: A --json needs to be specified.", err.Error())
+	sdj, err = getSdJSON("", "iglu:com.acme/event/jsonschema/1-0-0", "")
+	assert.Nil(sdj)
+	assert.NotNil(err)
+	assert.Equal("FATAL: A --json needs to be specified.", err.Error())
 
-  sdj, err = getSdJson("", "", "{\"e\":\"pv\"}")
-  assert.Nil(sdj)
-  assert.NotNil(err)
-  assert.Equal("FATAL: A --schema URI needs to be specified.", err.Error())
+	sdj, err = getSdJSON("", "", "{\"e\":\"pv\"}")
+	assert.Nil(sdj)
+	assert.NotNil(err)
+	assert.Equal("FATAL: A --schema URI needs to be specified.", err.Error())
 
-  sdj, err = getSdJson("", "iglu:com.acme/event/jsonschema/1-0-0", "{\"e\":\"pv\"}")
-  assert.Nil(err)
-  assert.NotNil(sdj)
-  assert.Equal("{\"data\":{\"e\":\"pv\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", sdj.String())
+	sdj, err = getSdJSON("", "iglu:com.acme/event/jsonschema/1-0-0", "{\"e\":\"pv\"}")
+	assert.Nil(err)
+	assert.NotNil(sdj)
+	assert.Equal("{\"data\":{\"e\":\"pv\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", sdj.String())
 
-  sdj, err = getSdJson("", "iglu:com.acme/event/jsonschema/1-0-0", "{\"e\"}")
-  assert.NotNil(err)
-  assert.Nil(sdj)
-  assert.Equal("invalid character '}' after object key", err.Error())
+	sdj, err = getSdJSON("", "iglu:com.acme/event/jsonschema/1-0-0", "{\"e\"}")
+	assert.NotNil(err)
+	assert.Nil(sdj)
+	assert.Equal("invalid character '}' after object key", err.Error())
 
-  sdj, err = getSdJson("{\"data\":{\"e\":\"pv\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", "", "")
-  assert.Nil(err)
-  assert.NotNil(sdj)
-  assert.Equal("{\"data\":{\"e\":\"pv\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", sdj.String())
+	sdj, err = getSdJSON("{\"data\":{\"e\":\"pv\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", "", "")
+	assert.Nil(err)
+	assert.NotNil(sdj)
+	assert.Equal("{\"data\":{\"e\":\"pv\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", sdj.String())
 
-  sdj, err = getSdJson("{\"data\":{\"e\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", "", "")
-  assert.NotNil(err)
-  assert.Nil(sdj)
-  assert.Equal("invalid character '}' after object key", err.Error())
+	sdj, err = getSdJSON("{\"data\":{\"e\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", "", "")
+	assert.NotNil(err)
+	assert.Nil(sdj)
+	assert.Equal("invalid character '}' after object key", err.Error())
 }
 
 // --- Tracker
 
 func TestInitTracker(t *testing.T) {
-  assert := assert.New(t)
-  trackerChan := make(chan int, 1)
+	assert := assert.New(t)
+	trackerChan := make(chan int, 1)
 
-  tracker := initTracker("com.acme", "myapp", "POST", "/tmp/test-1.db", trackerChan)
-  assert.NotNil(tracker)
-  assert.NotNil(tracker.Emitter)
-  assert.NotNil(tracker.Subject)
-  assert.Equal("myapp", tracker.AppId)
+	tracker := initTracker("com.acme", "myapp", "POST", "/tmp/test-1.db", trackerChan)
+	assert.NotNil(tracker)
+	assert.NotNil(tracker.Emitter)
+	assert.NotNil(tracker.Subject)
+	assert.Equal("myapp", tracker.AppId)
 }
 
 func TestTrackSelfDescribingEventGood(t *testing.T) {
-  assert := assert.New(t)
+	assert := assert.New(t)
 
-  // Setup HTTPMock
-  httpmock.Activate()
-  defer httpmock.DeactivateAndReset()
-  requests := []*http.Request{}
-  httpmock.RegisterResponder(
-    "GET",
-    "http://com.acme/i",
-    func(req *http.Request) (*http.Response, error) {
-      requests = append(requests, req)
-      return httpmock.NewStringResponse(200, ""), nil
-    },
-  )
+	// Setup HTTPMock
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	requests := []*http.Request{}
+	httpmock.RegisterResponder(
+		"GET",
+		"http://com.acme/i",
+		func(req *http.Request) (*http.Response, error) {
+			requests = append(requests, req)
+			return httpmock.NewStringResponse(200, ""), nil
+		},
+	)
 
-  // Setup Tracker
-  trackerChan := make(chan int, 1)
-  tracker := initTracker("com.acme", "myapp", "GET", "/tmp/test-2.db", trackerChan)
-  assert.NotNil(tracker)
+	// Setup Tracker
+	trackerChan := make(chan int, 1)
+	tracker := initTracker("com.acme", "myapp", "GET", "/tmp/test-2.db", trackerChan)
+	assert.NotNil(tracker)
 
-  // Make SDJ
-  schemaStr := "iglu:com.acme/event/jsonschema/1-0-0"
-  jsonDataMap, _ := stringToMap("{\"hello\":\"world\"}")
-  sdj := gt.InitSelfDescribingJson(schemaStr, jsonDataMap)
+	// Make SDJ
+	schemaStr := "iglu:com.acme/event/jsonschema/1-0-0"
+	jsonDataMap, _ := stringToMap("{\"hello\":\"world\"}")
+	sdj := gt.InitSelfDescribingJson(schemaStr, jsonDataMap)
 
-  // Send an event
-  statusCode := trackSelfDescribingEvent(tracker, trackerChan, sdj)
-  assert.Equal(200, statusCode)
-  assert.Equal(1, len(requests))
+	// Send an event
+	statusCode := trackSelfDescribingEvent(tracker, trackerChan, sdj)
+	assert.Equal(200, statusCode)
+	assert.Equal(1, len(requests))
 }
 
 func TestTrackSelfDescribingEventBad(t *testing.T) {
-  assert := assert.New(t)
+	assert := assert.New(t)
 
-  // Setup HTTPMock
-  httpmock.Activate()
-  defer httpmock.DeactivateAndReset()
-  requests := []*http.Request{}
-  httpmock.RegisterResponder(
-    "POST",
-    "http://com.acme/com.snowplowanalytics.snowplow/tp2",
-    func(req *http.Request) (*http.Response, error) {
-      requests = append(requests, req)
-      return httpmock.NewStringResponse(404, ""), nil
-    },
-  )
+	// Setup HTTPMock
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	requests := []*http.Request{}
+	httpmock.RegisterResponder(
+		"POST",
+		"http://com.acme/com.snowplowanalytics.snowplow/tp2",
+		func(req *http.Request) (*http.Response, error) {
+			requests = append(requests, req)
+			return httpmock.NewStringResponse(404, ""), nil
+		},
+	)
 
-  // Setup Tracker
-  trackerChan := make(chan int, 1)
-  tracker := initTracker("com.acme", "myapp", "POST", "/tmp/test-3.db", trackerChan)
-  assert.NotNil(tracker)
+	// Setup Tracker
+	trackerChan := make(chan int, 1)
+	tracker := initTracker("com.acme", "myapp", "POST", "/tmp/test-3.db", trackerChan)
+	assert.NotNil(tracker)
 
-  // Make SDJ
-  schemaStr := "iglu:com.acme/event/jsonschema/1-0-0"
-  jsonDataMap, _ := stringToMap("{\"hello\":\"world\"}")
-  sdj := gt.InitSelfDescribingJson(schemaStr, jsonDataMap)
+	// Make SDJ
+	schemaStr := "iglu:com.acme/event/jsonschema/1-0-0"
+	jsonDataMap, _ := stringToMap("{\"hello\":\"world\"}")
+	sdj := gt.InitSelfDescribingJson(schemaStr, jsonDataMap)
 
-  // Send an event
-  statusCode := trackSelfDescribingEvent(tracker, trackerChan, sdj)
-  assert.Equal(404, statusCode)
-  assert.Equal(1, len(requests))
+	// Send an event
+	statusCode := trackSelfDescribingEvent(tracker, trackerChan, sdj)
+	assert.Equal(404, statusCode)
+	assert.Equal(1, len(requests))
 }
 
 // --- Utilities
 
 func TestParseStatusCode(t *testing.T) {
-  assert := assert.New(t)
+	assert := assert.New(t)
 
-  result := parseStatusCode(200)
-  assert.Equal(0, result)
-  result = parseStatusCode(300)
-  assert.Equal(0, result)
-  result = parseStatusCode(404)
-  assert.Equal(4, result)
-  result = parseStatusCode(501)
-  assert.Equal(5, result)
-  result = parseStatusCode(600)
-  assert.Equal(1, result)
+	result := parseStatusCode(200)
+	assert.Equal(0, result)
+	result = parseStatusCode(300)
+	assert.Equal(0, result)
+	result = parseStatusCode(404)
+	assert.Equal(4, result)
+	result = parseStatusCode(501)
+	assert.Equal(5, result)
+	result = parseStatusCode(600)
+	assert.Equal(1, result)
 }
 
 func TestStringToMap(t *testing.T) {
-  assert := assert.New(t)
+	assert := assert.New(t)
 
-  m, err := stringToMap("{\"hello\":\"world\"}")
-  assert.Nil(err)
-  assert.NotNil(m)
-  assert.Equal("world", m["hello"])
-  assert.Equal(1, len(m))
+	m, err := stringToMap("{\"hello\":\"world\"}")
+	assert.Nil(err)
+	assert.NotNil(m)
+	assert.Equal("world", m["hello"])
+	assert.Equal(1, len(m))
 
-  m, err = stringToMap("{\"hello\"}")
-  assert.NotNil(err)
-  assert.Nil(m)
+	m, err = stringToMap("{\"hello\"}")
+	assert.NotNil(err)
+	assert.Nil(m)
 }
