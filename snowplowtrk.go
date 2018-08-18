610e8e3d0fbc648aee58b45d137bb964a90f73bc
jbeemster <joshua.a.beemster@gmail.com>
2018-08-18 09:49:15 +0200

    Bumped VERSION

--- snowplowtrk.go
+++ snowplowtrk.go
@@ -24,7 +24,7 @@ import (
 )
 
 const (
-	appVersion   = "0.1.0"
+	appVersion   = "0.2.0"
 	appName      = "snowplowtrk"
 	appUsage     = "Snowplow Analytics Tracking CLI"
 	appCopyright = "(c) 2016-2018 Snowplow Analytics, LTD"
